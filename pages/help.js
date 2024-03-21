3dc6ae5016d21053d9c18a5058a3a9000a7b9987
Sethormiston07 <Sethormiston@icloud.com>
2024-03-21 11:54:54 +0000

    added a title to help page

--- pages/help.js
+++ pages/help.js
@@ -5,19 +5,17 @@ import { Button } from "@chakra-ui/react";
 export default function HelpPage() {
     return (
         <>
-        <Navbar />
-        <NumberRange />
-        <Button style={{
-            color:"black"
-        }} colorScheme='whatsapp'><b>
-            Add to cart
-            </b>
+            <Navbar />
+            <h1>
+                Help
+            </h1>
+            <NumberRange />
+            <Button style={{
+                color: "black"
+            }} colorScheme='whatsapp'><b>
+                    Add to cart
+                </b>
             </Button>
-            
-    
-
-
-
         </>
     )
 }
