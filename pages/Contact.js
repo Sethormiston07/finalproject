34e27837d5de40118709b5cc253bb008d5260a25
Sethormiston07 <Sethormiston@icloud.com>
2024-05-07 09:21:56 +0100

    imported call and message buttons to contact page

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,6 +2,10 @@ import { Input } from '@chakra-ui/react'
 import {InputGroup} from '@chakra-ui/react'
 import {InputLeftAddon} from '@chakra-ui/react'
 import { Textarea } from '@chakra-ui/react'
+import {Stack} from '@chakra-ui/react'
+import {Button} from '@chakra-ui/react'
+import {EmailIcon} from '@chakra-ui/react'
+import {ArrowForwardIcon} from '@chakra-ui/react'
 
 export default function ContactPage() {
     return (
@@ -14,7 +18,14 @@ export default function ContactPage() {
             <Input type='tel' placeholder='phone number' />
             </InputGroup>
             <Textarea placeholder='Please Enter Your Comment' />
-
+            <Stack direction='row' spacing={4}>
+            <Button background={<EmailIcon />} colorScheme='teal' variant='solid'>
+                Email
+            </Button>
+            <Button background={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
+                Call us
+            </Button>
+            </Stack>
             </div>
         </>
     )
